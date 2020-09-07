a9b38829cf392195c3c6618064a59b25c11bc6f7
Akash Sharma <69471239+AkashSharmaClicks2000@users.noreply.github.com>
2020-09-07 21:56:09 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+D={"Abase":"to reduce or lower, as in rank, office, reputation, or estimation; humble; degrade.",
+   "Abate":"to reduce in amount, degree, intensity, etc.; lessen; diminish:",
+   "Abasia":"inability to walk due to a limitation or absence of muscular coordination.",
+   "Abator":"law a person who effects an abatement",
+   "Abaxial":"being or situated away from the axis:"}
+
+print("Search the words\n")
+
+w=input()
+print(D[w])
